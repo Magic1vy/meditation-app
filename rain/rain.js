0f72b72af4733c92d03f06901f707aec699d37fe
Valeriia Isaienko <lerika@MacBook-Pro.local>
2023-02-04 15:45:09 -0800

    first commit

---
+++ rain/rain.js
@@ -0,0 +1,37 @@
+const btn = document.querySelector("#myBtn");
+btn.addEventListener("click", ()=>{
+    document.querySelector("#player").play;
+    document.querySelector("#myVideo").play;
+})
+
+const timer = 5;
+let amountTime = timer * 60;
+
+function calculateTimer(){
+    const countdown = document.querySelector("#countdown");
+    let minutes = Math.floor(amountTime/60);
+    let seconds = amountTime % 60;
+
+    if(minutes < 10){
+        minutes = "0" + minutes;
+    }
+
+    if(seconds < 10){
+        seconds = "0" + seconds;
+    }
+
+    countdown.textContent = `${minutes} : ${seconds}`;
+    amountTime--;
+
+    if(amountTime < 0){
+        stopTimer();
+        amountTime = 0;
+    }
+
+}
+
+let timerId =  setInterval(calculateTimer,1000);
+
+function stopTimer(){
+    clearInterval(timerId);
+}
